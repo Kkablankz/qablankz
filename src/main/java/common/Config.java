2a0043d9b349686ff9a4d93fb549e552d55547f9
Kkablankz <kkablankz@gmail.com>
2021-08-19 12:16:49 +0600

    Add Clean and close browser

--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -7,4 +7,13 @@ public class Config {
      * MOZILLA
      * */
     public static final String BROWSER_AND_PLATFORM = "CHROME";
+    /*
+    * Clean browser cookies after each iteration
+    * */
+    public static final boolean CLEAR_COOKIES = true;
+    /*
+    *to keep the browser open after all scenario
+    * */
+    public static final boolean HOLD_BROWSER_OPEN = true;
+
 }

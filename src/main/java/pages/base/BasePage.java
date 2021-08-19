ef95e513991e6ceb5c65a67391cf44f4d916c208
Kkablankz <kkablankz@gmail.com>
2021-08-19 12:20:49 +0600

    Add method

--- src/main/java/pages/base/BasePage.java
+++ src/main/java/pages/base/BasePage.java
@@ -3,8 +3,15 @@ package pages.base;
 import org.openqa.selenium.WebDriver;
 
 public class BasePage {
+
     WebDriver driver;
     public BasePage(WebDriver driver) {
         this.driver = driver;
+    }
+    /*
+    * The method for navigating to a specific URL
+    * */
+    public void goToUrl(String url){
+        driver.get(url);
     }
 }

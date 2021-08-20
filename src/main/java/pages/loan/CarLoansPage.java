a3ef3d386305d3a5b82704d90d5caf2c81a34e57
Kkablankz <kkablankz@gmail.com>
2021-08-20 15:36:32 +0600

    Add new test for car leasing

--- src/main/java/pages/loan/CarLoansPage.java
+++ src/main/java/pages/loan/CarLoansPage.java
@@ -1,10 +1,17 @@
 package pages.loan;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CarLoansPage {
     WebDriver driver;
     public CarLoansPage(WebDriver driver) {
     this.driver = driver;
+    }
+    public final By tabAggrements = By.xpath("//*[@id=\"app\"]/div[2]/section/div[2]/a[2]/div");
+
+    public CarLoansPage selectAggrementsTab(){
+        driver.findElement(tabAggrements).click();
+        return this;
     }
 }
